d01642b0c135329fd5d177b292e4bdf313aac5a5
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-09-20 13:25:48 +0200

    improve login form submit user interaction

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,4 +2,8 @@ import {docReady} from "./main";
 
 docReady(function() {
     console.log('ouais re');
+    let form = document.getElementById('login-form');
+    form.addEventListener('submit',function(){
+        document.getElementById('button-login-submit').innerText = 'Connexion...';
+    })
 });
