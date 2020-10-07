dd38584229aacbe699d104992041c847c8c430d1
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-10-07 21:16:32 +0200

    add a dropdown allowing admins to filter on a service to see its tabular calendar view

--- assets/js/Screen/TabularCalendarScreen/TabularCalendarScreen.js
+++ assets/js/Screen/TabularCalendarScreen/TabularCalendarScreen.js
@@ -1,19 +1,34 @@
 import React, {useContext, useEffect, useState} from "react";
 import axios from 'axios';
 import './TabularCalendarScreen.css';
-import {Container, Header as SemanticHeader} from "semantic-ui-react";
+import {Container, Header as SemanticHeader, Select} from "semantic-ui-react";
 import TabularCalendar from "../../Component/TabularCalendar/TabularCalendar";
 import {SessionContext} from "../../Component/Context/session";
 import {displayErrorPopup} from "../../utils/error";
 import MyLoader from "../../Component/MyLoader/MyLoader";
+import {servicesToSelectable} from "../../utils/service";
 
 export default function TabularCalendarScreen(props){
     const userContext = useContext(SessionContext);
     const [datas, setDatas] = useState([{userName : userContext.user.first_name + ' ' + userContext.user.last_name}]);
     const [datasLoading, setDatasLoading] = useState(true);
+    const [serviceList, setServiceList] = useState([]);
+    const [service, setService] = useState(userContext.user.service.id);
 
     useEffect(() => {
-        axios.get('/api/holiday/service/tabular/'+userContext.user.service.id).then(result => {
+        axios.get('/api/service/all').then((result)=>{
+            setServiceList(servicesToSelectable(result.data));
+        }).catch((error)=>{
+            console.log(error);
+            displayErrorPopup(error);
+        });
+    },[]);
+
+    useEffect(() => {
+        if(datasLoading === false){
+            setDatasLoading(true);
+        }
+        axios.get('/api/holiday/service/tabular/'+service).then(result => {
             setDatas(result.data);
         }).catch(error => {
             displayErrorPopup(error);
@@ -21,11 +36,12 @@ export default function TabularCalendarScreen(props){
         }).finally(()=>{
             setDatasLoading(false);
         })
-    },[]);
+    },[service]);
 
     return(
         <Container className="custom-containers tabular-container">
             <SemanticHeader as="h1" className="text-center">Calendrier tabulaire</SemanticHeader>
+            {userContext.isAdmin && <div className="text-center"><Select placeholder="Filtre sur un service" value={service} onChange={(e,data) => setService(data.value)} options={serviceList}/></div>}
             {datasLoading ? (
                 <MyLoader size="big"/>
             ) : (
