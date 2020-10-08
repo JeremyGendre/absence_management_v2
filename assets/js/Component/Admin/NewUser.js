bd59daf1f84bc53c3299f31775359b8ae71e7626
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-10-08 21:21:54 +0200

    empty user field after creating a new user

--- assets/js/Component/Admin/NewUser.js
+++ assets/js/Component/Admin/NewUser.js
@@ -9,16 +9,18 @@ import {isBadResult} from "../../utils/server";
 
 const MySwal = withReactContent(Swal);
 
+const initialUserState = {
+    lastName: '',
+    firstName: '',
+    service: null,
+    title: '',
+    email: '',
+    username: '',
+    password: '',
+};
+
 export default function NewUser(props){
-    const [newUser,setNewUser] = useState({
-        lastName: '',
-        firstName: '',
-        service: null,
-        title: '',
-        email: '',
-        username: '',
-        password: '',
-    });
+    const [newUser,setNewUser] = useState(initialUserState);
     const [submitting,setSubmitting] = useState(false);
     const [serviceLoading,setServiceLoading] = useState(true);
     const [services,setServices] = useState([]);
@@ -56,6 +58,7 @@ export default function NewUser(props){
                     if(errorMessage !== ''){
                         displayErrorPopup(errorMessage);
                     }else{
+                        setNewUser(initialUserState);
                         MySwal.fire({
                             icon:'success',
                             title:'Utilisateur créé',
@@ -116,22 +119,22 @@ export default function NewUser(props){
                 )}
 
                 <Form.Group>
-                    <Form.Input fluid icon='user outline' iconPosition='left' label="Nom" required placeholder='Nom de famille' defaultValue={newUser.lastName} onChange={(e,data) => setNewUser({...newUser,lastName:data.value})} width={8}/>
-                    <Form.Input fluid icon='user outline' iconPosition='left' label="Prénom" required placeholder='Prénom' defaultValue={newUser.firstName} onChange={(e,data) => setNewUser({...newUser,firstName:data.value})} width={8}/>
-                    <Form.Input fluid icon='at' iconPosition='left' type="email" required label="Email" placeholder='Email' defaultValue={newUser.email} onChange={(e,data) => setNewUser({...newUser,email:data.value})} width={8}/>
+                    <Form.Input fluid icon='user outline' iconPosition='left' label="Nom" required placeholder='Nom de famille' value={newUser.lastName} onChange={(e,data) => setNewUser({...newUser,lastName:data.value})} width={8}/>
+                    <Form.Input fluid icon='user outline' iconPosition='left' label="Prénom" required placeholder='Prénom' value={newUser.firstName} onChange={(e,data) => setNewUser({...newUser,firstName:data.value})} width={8}/>
+                    <Form.Input fluid icon='at' iconPosition='left' type="email" required label="Email" placeholder='Email' value={newUser.email} onChange={(e,data) => setNewUser({...newUser,email:data.value})} width={8}/>
                 </Form.Group>
                 <Form.Group>
                     <Form.Input fluid icon='user' iconPosition='left' label="Nom d'utilisateur" required
-                                placeholder="Nom d'utilisateur" defaultValue={newUser.username}
+                                placeholder="Nom d'utilisateur" value={newUser.username}
                                 onChange={(e,data) => setNewUser({...newUser,username:data.value})} width={6}/>
                     <Form.Input fluid icon='user secret' iconPosition='left' label="Mot de passe (provisoire)" required
                                 placeholder='Mot de passe' type="password" minLength={6} maxLength={100}
-                                defaultValue={newUser.password} onChange={(e,data) => setNewUser({...newUser,password:data.value})} width={6}/>
+                                value={newUser.password} onChange={(e,data) => setNewUser({...newUser,password:data.value})} width={6}/>
                 </Form.Group>
                 <Form.Group>
                     <Form.Select fluid label="Service" required options={services} onChange={(e,data) =>setNewUser({...newUser,service:data.value})} value={newUser.service} width={8}/>
                     <Form.Input fluid icon="id badge" iconPosition='left' label="Intitulé de poste" required
-                                placeholder='Intitulé de poste' defaultValue={newUser.title} onChange={(e,data) => setNewUser({...newUser,title:data.value})} width={8}/>
+                                placeholder='Intitulé de poste' value={newUser.title} onChange={(e,data) => setNewUser({...newUser,title:data.value})} width={8}/>
                 </Form.Group>
                 <Form.Group>
                     <Form.Button className="submit-button-container" primary disabled={submitting} loading={submitting}>Enregistrer</Form.Button>
